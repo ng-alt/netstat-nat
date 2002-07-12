43ed7cad1a7f577370ff6528f89ab227606e0f33
Gerrit Pusher <pusher@localhost>
2002-07-13 04:05:54 +0800

    Import netstat-nat v1.2

--- netstat-nat.c
+++ netstat-nat.c
@@ -1,9 +1,27 @@
 /*
 #-------------------------------------------------------------------------------
 #                                                                                                                         
-# $Id: netstat-nat.c,v 1.8 2002/07/07 20:27:47 mardan Exp $     
+# $Id: netstat-nat.c,v 1.11 2002/07/12 20:05:54 mardan Exp $     
 #       
 # $Log: netstat-nat.c,v $
+# Revision 1.11  2002/07/12 20:05:54  mardan
+# Added argument for extended view of hostnames.
+# Moved display-code into one function.
+# Removed most unnessacery code.
+# Updated README
+#
+# Revision 1.10  2002/07/10 19:58:33  mardan
+# Added filtering by destination-host, re-arranged some code to work properly.
+# Tested DNAT icmp and udp.(pls report if any bugs occur)
+# Fixed a few declaration bugs.
+#
+# Revision 1.9  2002/07/09 20:00:36  mardan
+# Added fully DNAT support (udp & icmp not fully tested yet, but should work),
+# including argument support for (S)(D)NAT selection.
+# Re-arranged layout code, can possible merged into one function.
+# Some few minor changes.
+# Started to work on destination-host selection.
+#
 # Revision 1.8  2002/07/07 20:27:47  mardan
 # Added display by source host/IP.
 # Made a few fixes/changes.
@@ -68,34 +86,38 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-//#include <sys/types.h>
-//#include <errno.h>
-
-static char const rcsid[] = "$Id: netstat-nat.c,v 1.8 2002/07/07 20:27:47 mardan Exp $";
+static char const rcsid[] = "$Id: netstat-nat.c,v 1.11 2002/07/12 20:05:54 mardan Exp $";
 static char const rcsname[] = "$Author: mardan $";
-static int RESOLVE;
+static int RESOLVE = 1;
 static char PROTOCOL[4] = "";
-static char SRC_IP[15] = "";
-
+static char SRC_IP[35] = "";
+static char DST_IP[35] = "";
+static int SNAT = 1;
+static int DNAT = 1;
+static int EXT_VIEW = 0;
 int main(argc, argv)
 int argc;
+
 char *argv[];
     {
     int c;
-    const char *args = "hnp:s:";
+    const char *args = "hnp:s:d:SDx";
     int ret;
     int argerr;
     FILE *f;
     char line[200];
-    RESOLVE = 1;
-    // check paramters
+    // check parameters
     while ((c = getopt(argc, argv, args)) != -1 ) {
 	switch (c) {
 	case 'h':
-	    printf("args: -h: prints this help\n");
-	    printf("      -n: don't resolve IPs\n");
-	    printf("      -p tcp | udp | icmp: prints only selected protocol\n");
-	    printf("      -s <source-host>: prints only connections by NATed IP/hostname\n");
+	    printf("args: -h: displays this help\n");
+	    printf("      -n: don't resolve hostnames\n");
+	    printf("      -p tcp | udp | icmp  : display selected protocol\n");
+	    printf("      -s <source-host>     : display connections by source\n");
+	    printf("      -d <destination-host>: display connections by destination\n");
+	    printf("      -S: display SNAT connections\n");
+	    printf("      -D: display DNAT connections (default: SNAT & DNAT)\n"); 
+	    printf("      -x: extended hostnames view\n");
 	    return 1;
 	case 'n':
 	    RESOLVE = 0;
@@ -107,18 +129,35 @@ char *argv[];
 	    strcpy (SRC_IP, optarg);
 	    lookup_ip(SRC_IP);
 	    break;
+	case 'd':
+	    strcpy (DST_IP, optarg);
+	    lookup_ip(DST_IP);
+	    break;    
+	case 'S':
+	    DNAT = 0;
+	    break;
+	case 'D':
+	    SNAT = 0;
+	    break;
+	case 'x':
+	    EXT_VIEW = 1;
+	    break;
 	}
     }
     
     // some checking for IPTables and read file
     f=fopen("/proc/net/ip_conntrack","r");
     if (!f) {
-	printf("Make sure IPTables is enabled by kernel or modules.\n");
+	printf("Make sure netfilter/IPtables is enabled by kernel or modules.\n");
 	return 1;
 	}
     
     // process conntrack table
-    printf("Proto NATed Address            Foreign Address          Ports        State\n");
+    if (!EXT_VIEW) {
+	printf("%-6s%-25s%-25s%-13s%-6s","Proto","NATed Address","Foreign Address","Ports","State\n");
+    } else {
+	printf("%-6s%-35s%-35s%-13s%-6s","Proto","NATed Address","Foreign Address","Ports","State\n");
+	}
     while (fgets(line,1000,f)!=NULL) {
 	if ((!strcmp(PROTOCOL, "tcp")) || (!strcmp(PROTOCOL, ""))) {
 	    if(match(line, "tcp")) {
@@ -151,20 +190,13 @@ char *argv[];
     return 0;
     }
 
-
 // -- NATed protocols
 
 // NATed tcp protocol
 int protocol_tcp(char *line)
     {
-    char protocol[6]="";
-    char src_ip[25]="", dst_ip[25]="";
-    char ip_buffer[15]="", gen_buffer[25]="", port_buffer[5]=""; 
-    char src_port[11]="", dst_port[11]="";
-    char ports[15]="";
     char *token;
-    char *split;
-    char *buf[30];
+    char *buf[35];
     int count;
     token = strtok(line," ");
     count = 0;
@@ -181,46 +213,25 @@ int protocol_tcp(char *line)
 	extract_ip(buf[9]);
 	extract_ip(buf[5]);
 	extract_ip(buf[8]);
-	}
-    else {
-	buf[4]="";
-	buf[5]="";
-	buf[8]="";
-	buf[9]="";
-	}
-    if ((!strcmp(buf[4],buf[9])==0) && (strcmp(buf[5],buf[8])==0)) {		
-	if (check_if_source(buf[4])) {
-	    // protocol
-	    strcpy(gen_buffer, buf[0]);
-	    protocol_layout(gen_buffer);
-	    // source host
-	    strcpy(gen_buffer, buf[4]);
-	    sourceip_layout(gen_buffer);
-	    // destination host
-	    strcpy(gen_buffer, buf[5]);
-	    destip_layout(gen_buffer);
-	    //port_layout
-	    strcpy(src_port, buf[6]);
-	    strcpy(dst_port, buf[10]);
-	    port_layout(src_port, dst_port);
-	    // connection status
-	    printf("%-10s", buf[3]);
-	    // EOL
-	    printf("\n");
+	if (SNAT) {
+	    if ((!strcmp(buf[4],buf[9])==0) && (strcmp(buf[5],buf[8])==0)) {		
+		check_src_dst(buf[0],buf[4],buf[8],buf[6],buf[7],buf[3]);
+		}
+	    }
+	if (DNAT) {
+	    if ((strcmp(buf[4],buf[9])==0) && (!strcmp(buf[5],buf[8])==0)) {		
+		check_src_dst(buf[0],buf[4],buf[8],buf[6],buf[7],buf[3]);
+		}
 	    }
 	}
+    return(1);
     }
 
 // NATed udp protocol
-int protocol_udp(char *line) {
-    char protocol[6]="";
-    char src_ip[25]="", dst_ip[25]="";
-    char ip_buffer[15]="", gen_buffer[25]="", port_buffer[5]=""; 
-    char src_port[11]="", dst_port[11]="";
-    char ports[15]="";
+int protocol_udp(char *line) 
+    {
     char *token;
-    char *split;
-    char *buf[30];
+    char *buf[35];
     int count;
     token = strtok(line," ");
     count = 0;
@@ -237,47 +248,24 @@ int protocol_udp(char *line) {
 	extract_ip(buf[3]);
 	extract_ip(buf[6]);
 	extract_ip(buf[7]);
-	}
-    else {
-        buf[2]="";
-        buf[3]="";
-        buf[6]="";
-        buf[7]="";
-        }
-    
-    if ((!strcmp(buf[2],buf[7])==0) && (strcmp(buf[3],buf[6])==0)) {	
-	if (check_if_source(buf[2])) {
-	    // protocol
-	    strcpy(gen_buffer, buf[0]);
-	    protocol_layout(gen_buffer);
-	    // source host
-	    strcpy(gen_buffer, buf[2]);
-	    sourceip_layout(gen_buffer);
-	    // destination host
-	    strcpy(gen_buffer, buf[6]);
-	    destip_layout(gen_buffer);
-	    //port_layout
-	    strcpy(src_port, buf[4]);
-	    strcpy(dst_port, buf[5]);
-	    port_layout(src_port, dst_port);
-	    // connection status
-	    printf("%-10s", " ");
-	    // EOL
-	    printf("\n");	    
+	if (SNAT) {
+	    if ((!strcmp(buf[2],buf[7])==0) && (strcmp(buf[3],buf[6])==0)) {	
+		check_src_dst(buf[0],buf[2],buf[6],buf[4],buf[5]," ");
+		}    
 	    }
-	}    
+	if (DNAT) {
+	    if ((strcmp(buf[2],buf[7])==0) && (!strcmp(buf[3],buf[6])==0)) {	
+		check_src_dst(buf[0],buf[2],buf[6],buf[4],buf[5]," ");
+		}    
+	    }	
+	}
+    return(1);
     }
 
-
-int protocol_udp_ass(char *line) {
-    char protocol[6]="";
-    char src_ip[25]="", dst_ip[25]="";
-    char ip_buffer[15]="", gen_buffer[25]="", port_buffer[5]=""; 
-    char src_port[11]="", dst_port[11]="";
-    char ports[15]="";
+int protocol_udp_ass(char *line) 
+    {
     char *token;
-    char *split;
-    char *buf[30];
+    char *buf[35];
     int count;
     token = strtok(line," ");
     count = 0;
@@ -294,47 +282,24 @@ int protocol_udp_ass(char *line) {
 	extract_ip(buf[4]);
 	extract_ip(buf[7]);
 	extract_ip(buf[8]);
-	}
-    else {
-        buf[3]="";
-        buf[4]="";
-        buf[7]="";
-        buf[8]="";
-        }
-    
-    if ((!strcmp(buf[3],buf[8])==0) && (strcmp(buf[4],buf[7])==0)) {	
-	if (check_if_source(buf[3])) {
-	    // protocol
-	    strcpy(gen_buffer, buf[0]);
-	    protocol_layout(gen_buffer);
-	    // source host
-	    strcpy(gen_buffer, buf[3]);
-	    sourceip_layout(gen_buffer);
-	    // destination host
-	    strcpy(gen_buffer, buf[7]);
-	    destip_layout(gen_buffer);
-	    //port_layout
-	    strcpy(src_port, buf[5]);
-	    strcpy(dst_port, buf[6]);
-	    port_layout(src_port, dst_port);
-	    // connection status
-	    printf("%-10s", buf[11]);
-	    // EOL
-	    printf("\n");	    
+	if(SNAT) {
+	    if ((!strcmp(buf[3],buf[8])==0) && (strcmp(buf[4],buf[7])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[7],buf[5],buf[6],buf[11]);
+		}    
 	    }
-	}    
+	if(DNAT) {
+	    if ((strcmp(buf[3],buf[8])==0) && (!strcmp(buf[4],buf[7])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[7],buf[5],buf[6],buf[11]);
+		}    
+	    }
+	}
+    return(1);
     }
 
-
-int protocol_udp_unr(char *line) {
-    char protocol[6]="";
-    char src_ip[25]="", dst_ip[25]="";
-    char ip_buffer[15]="", gen_buffer[25]="", port_buffer[5]=""; 
-    char src_port[11]="", dst_port[11]="";
-    char ports[15]="";
+int protocol_udp_unr(char *line) 
+    {
     char *token;
-    char *split;
-    char *buf[30];
+    char *buf[35];
     int count;
     token = strtok(line," ");
     count = 0;
@@ -351,51 +316,29 @@ int protocol_udp_unr(char *line) {
 	extract_ip(buf[4]);
 	extract_ip(buf[8]);
 	extract_ip(buf[9]);
-	}
-    else {
-        buf[3]="";
-        buf[4]="";
-        buf[8]="";
-        buf[9]="";
-        }
-    
-    if ((!strcmp(buf[3],buf[9])==0) && (strcmp(buf[4],buf[8])==0)) {	
-	if (check_if_source(buf[3])) {
-	    // protocol
-	    strcpy(gen_buffer, buf[0]);
-	    protocol_layout(gen_buffer);
-	    // source host
-	    strcpy(gen_buffer, buf[3]);
-	    sourceip_layout(gen_buffer);
-	    // destination host
-	    strcpy(gen_buffer, buf[8]);
-	    destip_layout(gen_buffer);
-	    //port_layout
-	    strcpy(src_port, buf[5]);
-	    strcpy(dst_port, buf[6]);
-	    port_layout(src_port, dst_port);
-	    // connection status
-	    printf("%-10s", buf[7]);
-	    // EOL
-	    printf("\n");	    
+	if (SNAT) {
+	    if ((!strcmp(buf[3],buf[9])==0) && (strcmp(buf[4],buf[8])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[8],buf[5],buf[6],buf[7]);
+		}    
 	    }
-	}    
+	if (DNAT) {
+	    if ((strcmp(buf[3],buf[9])==0) && (!strcmp(buf[4],buf[8])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[8],buf[5],buf[6],buf[7]);
+		}    
+	    }
+	}
+    return(1);
     }
 
-
-
 // NATed icmp protocol
-int protocol_icmp_unr(char *line) {
-    char protocol[6]="";
-    char src_ip[25]="", dst_ip[25]="";
-    char ip_buffer[15]="", gen_buffer[25]="", port_buffer[5]=""; 
-    char ports[15]="";
+int protocol_icmp_unr(char *line) 
+    {
     char *token;
-    char *split;
-    char *buf[30];
+    char *buf[35];
     int count;
     token = strtok(line," ");
     count = 0;
+    
     while(token != NULL) {
 	buf[count] = token;
 	if(!strlen(buf[count]))
@@ -408,46 +351,28 @@ int protocol_icmp_unr(char *line) {
         extract_ip(buf[10]);
         extract_ip(buf[4]);
         extract_ip(buf[9]);
-        }
-    else {
-        buf[3]="";
-        buf[4]="";
-        buf[9]="";
-        buf[10]="";
-        }
-    
-    if ((!strcmp(buf[3],buf[10])==0) && (strcmp(buf[4],buf[9])==0)) {	
-        if (check_if_source(buf[3])) {
-	    // protocol
-    	    strcpy(gen_buffer, buf[0]);
-    	    protocol_layout(gen_buffer);
-	    // source host
-    	    strcpy(gen_buffer, buf[3]);
-    	    sourceip_layout(gen_buffer);
-    	    // destination host
-    	    strcpy(gen_buffer, buf[4]);
-    	    destip_layout(gen_buffer);
-    	    // port layout
-    	    printf("%-13s"," ");
-    	    // connection status
-    	    printf("%-10s", buf[8]);
-    	    // EOL
-    	    printf("\n");	    
-    	    }
-	}    
+        if (SNAT) {
+	    if ((!strcmp(buf[3],buf[10])==0) && (strcmp(buf[4],buf[9])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[9]," "," ",buf[8]);
+    		}
+	    }
+	if (DNAT) {
+	    if ((strcmp(buf[3],buf[10])==0) && (!strcmp(buf[4],buf[9])==0)) {	
+    		check_src_dst(buf[0],buf[3],buf[9]," "," ",buf[8]);
+		}    
+	    }
+	}
+    return(1);
     }
 
-int protocol_icmp_rep(char *line) {
-    char protocol[6]="";
-    char src_ip[25]="", dst_ip[25]="";
-    char ip_buffer[15]="", gen_buffer[25]="", port_buffer[5]=""; 
-    char ports[15]="";
+int protocol_icmp_rep(char *line) 
+    {
     char *token;
-    char *split;
-    char *buf[30];
+    char *buf[35];
     int count;
     token = strtok(line," ");
     count = 0;
+    
     while(token != NULL) {
 	buf[count] = token;
 	if(!strlen(buf[count]))
@@ -460,90 +385,99 @@ int protocol_icmp_rep(char *line) {
 	extract_ip(buf[9]);
 	extract_ip(buf[4]);
 	extract_ip(buf[8]);
-	}
-    else {
-	buf[3]="";
-	buf[4]="";
-	buf[8]="";
-	buf[9]="";
-	}
-	
-    if ((!strcmp(buf[3],buf[9])==0) && (strcmp(buf[4],buf[8])==0)) {	
-	if (check_if_source(buf[3])) {
-	    // protocol
-	    strcpy(gen_buffer, buf[0]);
-	    protocol_layout(gen_buffer);
-	    // source host
-	    strcpy(gen_buffer, buf[3]);
-	    sourceip_layout(gen_buffer);
-	    // destination host
-	    strcpy(gen_buffer, buf[4]);
-	    destip_layout(gen_buffer);
-	    // port layout
-	    printf("%-13s"," ");
-	    // connection status
-	    printf("%-10s", "[REPLIED]");
-	    // EOL
-	    printf("\n");	    
+	if (SNAT) {
+	    if ((!strcmp(buf[3],buf[9])==0) && (strcmp(buf[4],buf[8])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[9]," "," ","[REPLIED]");
+		}
+	    }
+	if (DNAT) {
+	    if ((strcmp(buf[3],buf[9])==0) && (!strcmp(buf[4],buf[8])==0)) {	
+		check_src_dst(buf[0],buf[3],buf[9]," "," ","[REPLIED]");
+		}
 	    }
 	}
+    return(1);
     }
+    
 // -- End of NATed protocols
 
 // -- Internal used functions
-int extract_ip(char *gen_buffer) {
+// Check filtering by source and destination IP
+int check_src_dst(char *protocol, char *src_ip, char *dst_ip, char *src_port, char *dst_port, char *status) 
+    {
+    if ((check_if_source(src_ip)) && (strcmp(DST_IP,"")==0)) {
+	print_connection(protocol,src_ip,dst_ip,src_port,dst_port,status);
+	}
+    else if ((check_if_destination(dst_ip)) && (strcmp(SRC_IP,"")==0)) {
+	print_connection(protocol,src_ip,dst_ip,src_port,dst_port,status);
+	}
+    else if ((check_if_destination(dst_ip)) && (check_if_source(src_ip))) {
+	print_connection(protocol,src_ip,dst_ip,src_port,dst_port,status);
+	}
+    return(1);
+    }
+
+int print_connection(char *protocol, char *src_ip, char *dst_ip, char *src_port, char *dst_port, char *status)  
+    {
+    char gen_buffer[35] = "";
+    char ports[12] = "";
+    char *split;
+    //protocol_layout(protocol);
+    memcpy(gen_buffer, protocol, 5);
+    printf("%-6s", gen_buffer);
+    //sourceip_layout(src_ip);
+    if (RESOLVE) {
+        lookup_hostname(src_ip);}
+    if (!EXT_VIEW) {
+	memcpy(gen_buffer, src_ip, 24);
+	printf("%-25s", gen_buffer);
+    } else {
+	memcpy(gen_buffer, src_ip, 34);
+	printf("%-35s", gen_buffer);
+	}
+    //destip_layout(dst_ip);
+    if (RESOLVE) {
+        lookup_hostname(dst_ip);}
+    if (!EXT_VIEW) {
+	memcpy(gen_buffer, dst_ip, 24);
+	printf("%-25s", gen_buffer);
+    } else {
+	memcpy(gen_buffer, dst_ip, 34);
+	printf("%-35s", gen_buffer);
+	}
+    //port_layout(src_port, dst_port);
+    if ((match(src_port, "=")) && (match(dst_port, "="))) {
+	split = strtok(src_port,"=");
+	split = strtok(NULL,"=");
+	src_port = split;
+	// destination port
+	strncpy(ports, src_port, 5); 
+	split = strtok(dst_port,"=");
+	split = strtok(NULL,"=");
+	dst_port = split;
+	// port layout
+	strcat(ports, "->");
+	strncat(ports, dst_port, 5); 
+	}
+    printf("%-13s", ports);
+    // status layout
+    printf("%-10s", status);
+    printf("\n");
+    return(1);
+    }
+
+int extract_ip(char *gen_buffer) 
+    {
     char *split;
     split = strtok(gen_buffer,"=");
     split = strtok(NULL,"=");
     strcpy(gen_buffer,split);
+    return(1);
     }
 
-int protocol_layout(char *gen_buffer) {
-    char protocol[6]="";
-    memcpy(protocol, gen_buffer, 5);
-    printf("%-6s", protocol);
-    }
-
-
-int sourceip_layout(char *gen_buffer) {
-    char src_ip[30]="";
-    if (RESOLVE) {
-        lookup_hostname(gen_buffer);}
-    memcpy(src_ip, gen_buffer, 24);
-    printf("%-25s", src_ip);
-    }
-
-
-int destip_layout(char *gen_buffer) {
-    char dst_ip[30]="";
-    if (RESOLVE) {
-        lookup_hostname(gen_buffer);}
-    memcpy(dst_ip, gen_buffer, 24);
-    printf("%-25s", dst_ip);
-    }
-
-    
-int port_layout(char *src_port, char *dst_port) {
-    char ports[12]="";
-    char *split;
-    // source port
-    split = strtok(src_port,"=");
-    split = strtok(NULL,"=");
-    src_port = split;
-    // destination port
-    strncpy(ports, src_port, 5); 
-    split = strtok(dst_port,"=");
-    split = strtok(NULL,"=");
-    dst_port = split;
-    // port layout
-    strcat(ports, "->");
-    strncat(ports, dst_port, 5); 
-    printf("%-13s", ports);
-    }    
-
-
-int lookup_hostname(char *r_host) {
-    u_int addr;
+int lookup_hostname(char *r_host) 
+    {
+    int addr;
     struct hostent *hp;
     char **p;
     char *hostname;
@@ -563,7 +497,6 @@ int lookup_hostname(char *r_host) {
     return (0);
     }
 
-
 int lookup_ip(char *hostname)
     {
     char *ip;
@@ -580,34 +513,41 @@ int lookup_ip(char *hostname)
     return(1);
     }
 
-
-int match(char *string, char *pattern) {
+int match(char *string, char *pattern) 
+    {
     int i;
     regex_t re;
     char buf[200];
     i=regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB);
     if (i != 0) {
 	(void)regerror(i,&re,buf,sizeof buf);
-	//printf("%s\n",buf);
 	return(0);                       /* report error */
 	}
     i = regexec(&re, string, (size_t) 0, NULL, 0);
     regfree(&re);
     if (i != 0) {
 	(void)regerror(i,&re,buf,sizeof buf);
-	//printf("%s\n",buf);
 	return(0);                       /* report error */
 	}
     return(1);
     }
 
-int check_if_source(char *host) {
+int check_if_source(char *host) 
+    {
     if ((strcmp(host,SRC_IP)==0) || (strcmp(SRC_IP, "")==0)) {
 	return(1);
 	}
     return(0);
     }
-    
+
+int check_if_destination(char *host) 
+    {
+    if ((strcmp(host,DST_IP)==0) || (strcmp(DST_IP, "")==0)) {
+	return(1);
+	}
+    return(0);
+    }
+
 // -- End of internal used functions
 
 // -- The End --
